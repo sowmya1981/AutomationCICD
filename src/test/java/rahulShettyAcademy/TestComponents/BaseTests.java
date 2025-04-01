c37001b5b77e91f229f20140da683e1ee52b87d7
sowmya <depurusowmya@gmail.com>
2025-04-02 00:26:52 +1300

    updated base tests

--- src/test/java/rahulShettyAcademy/TestComponents/BaseTests.java
+++ src/test/java/rahulShettyAcademy/TestComponents/BaseTests.java
@@ -1,31 +1,3 @@
-package rahulShettyAcademy.TestComponents;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Properties;
-
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import rahulShettyAcademy.PageObjects.LandingPage;
-
 public class BaseTests {
 
 	public WebDriver driver;
@@ -51,8 +23,7 @@ public class BaseTests {
 
 		if (browserName.contains("chrome")) {
 			ChromeOptions options = new ChromeOptions(); // This is to run the chrome in headless
-			WebDriverManager.chromedriver().setup();
-			
+					
 			if(browserName.contains("headless")) {
 			options.addArguments("headless");
 			}
@@ -62,9 +33,7 @@ public class BaseTests {
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			
 		} else if (browserName.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver",
-					"/Users/sowmy/Downloads/geckodriver-v0.36.0-win-aarch64/geckodriver");
-			WebDriverManager.firefoxdriver().setup();
+			
 			driver = new FirefoxDriver();
 			// firefox
 		}
